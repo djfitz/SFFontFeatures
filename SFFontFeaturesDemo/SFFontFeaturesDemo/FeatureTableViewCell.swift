640e1451f4832e42ed1fb73ecf2c37955e412967
Doug Hill <djfitz@users.noreply.github.com>
2017-02-16 08:55:46 -0800

    Update FeatureTableViewCell.swift

--- SFFontFeaturesDemo/SFFontFeaturesDemo/FeatureTableViewCell.swift
+++ SFFontFeaturesDemo/SFFontFeaturesDemo/FeatureTableViewCell.swift
@@ -19,16 +19,4 @@ class FeatureTableViewCell: UITableViewCell {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
